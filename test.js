006b199895c19332ebb06e1d5d7ed62763065460
jorge-diaz-m <jorge-d@ysteng.com>
2025-07-03 15:46:44 +0900

    update 10

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 const myVariable = 10;
 function calculateSum(a,b){
-    return a+b;
-    
+    return a+b; 
 }
 if(myVariable > 5){console.log("Greater than 5");}
-else{console.log("Not greater than 5");}
+else{console.log("Not greater than 5")};
