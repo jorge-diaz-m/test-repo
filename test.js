379b8e259bc7b32b9b4c0e06cd1828f9a03945c4
jorge-diaz-m <jorge-d@ysteng.com>
2025-07-04 11:02:17 +0900

    test for black

--- test.js
+++ test.js
@@ -3,4 +3,4 @@ function calculateSum(a,b){
     return a+b; 
 }
 if(myVariable > 5){console.log("Greater than 5");}
-else{console.log("Not greater than 5")};
+else{console.log("Not greater than 5");}
